576ea18372034ab5ec35c7a368c66b95da0734cd
DanielSWaite190 <danielwaite190@gmail.com>
2022-02-22 17:36:05 -0700

    Failed: Tried to use time stamps from MC logg file

--- server_backup_skript.py
+++ server_backup_skript.py
@@ -40,32 +40,15 @@ def main(args):
             match = re.search("\[\d+:\d+:\d+\]\s\[Server thread/INFO]:\s.+\[/\d+.\d+.\d+.\d:\d+\]\slogged in", line)
             if match:
                 p = re.search(":\s.+\[/", line) #Regex for username string
-                t = re.search("\d+:\d+:\d+", line) #Regex for time string
-
                 player = p.group() #Saving username string as player
-                time_string = t.group() #Saving time string as time_string
-
-                hours = int(time_string[0:2])
-                minute = int(time_string[3:5])
-                second = int(time_string[6:8])
-
-                time_stamp = datetime.timedelta(hours=hours, minutes=minute, seconds=second)
-                fiv_minets = datetime.timedelta(hours=17, minutes=55, seconds=18)
-
-                # time_stamp = datetime.datetime(hours, minute, second)
-                # fiv_minets = datetime.datetime(17, 55, 18)
-
-                players_model.update({player:time_stamp})
+                players_model.update({player:datetime.datetime.now()})
                 # os.makedirs(os.path.join(parsed_args.backup_location, f"i_am_daniel"))
 
-    # print(time_stamp)
-    # print(fiv_minets)
 
     x = players_model.get(": Undeflned[/'")
 
-    # print(fiv_minets-time_stamp)
     # print(players_model.get(": Undeflned[/'"))
-    print(fiv_minets - time_stamp)
+    print(players_model)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
